bbed43edb08cdeadd373da2814376ce3a8e123bd
Saffa Dhiya Ur Rahma <saffadhiyaa1012@gmail.com>
2025-10-20 09:41:00 +0700

    app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -2,35 +2,33 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-model = joblib.load("model_klasifikasi_lemon.joblib")
-
 st.set_page_config(
-    page_title="Klasifikasi Lemon",
-    page_icon="🍋"
-)
+    page_title="Belajar Klasifikasi Lemon",
+    page_icon=":lemon:")
 
-st.title("🍋 Klasifikasi Lemon")
-st.markdown("Aplikasi untuk mengklasifikasikan lemon menjadi kategori **Bagus**, **Sedang**, atau **Jelek**.")
+model = joblib.load("model_Klasifikasi_lemon.joblib")
 
-diameter = st.slider("Diameter", 40.0, 75.0, 50.0)
-berat = st.slider("Berat", 60.0, 150.0, 100.0)
-tebal_kulit = st.slider("Tebal Kulit", 3.0, 7.0, 3.5)
-kadar_gula = st.slider("Kadar Gula", 5.0, 10.0, 6.0)
+st.title(":lemon: Belajar Klasifikasi Lemon")
+st.markdown("Aplikasi machine learning classification untuk memprediksi kualitas lemon")
 
-asal_daerah = st.selectbox("Asal Daerah", ["California", "Malang", "Medan"], index=0)
-warna = st.selectbox("Warna", ["Hijau pekat", "Kuning kehijauan", "Kuning cerah"], index=1)
-musim_panen = st.selectbox("Musim Panen", ["Puncak", "Akhir", "Awal"], index=0)
+diameter = st.slider("Diameter", 40.0, 70.0,)
+berat = st.slider("Berat", 60.0, 150.0,)
+tebal_kulit = st.slider("Tebal Kulit", 3.0, 7.0,)
+kadar_gula = st.slider("Kadar Gula", 6.0, 9.0,)
+asal_daerah = st.pills("Asal Daerah", ["California", "Malang", "Medan"], default="California")
+warna = st.pills("Warna", ["Hijau pekat", "Kuning kehijauan", "Kuning cerah"], default="Hijau pekat")
+musim_panen = st.pills("Musim Panen", ["Awal", "Puncak", "Akhir"], default="Awal")
 
 if st.button("Prediksi", type="primary"):
-  
-    data = pd.DataFrame([[diameter, berat, tebal_kulit, kadar_gula, asal_daerah, warna, musim_panen]],
-                        columns=["diameter", "berat", "tebal_kulit", "kadar_gula", "asal_daerah", "warna", "musim_panen"])
-    
-    prediksi = model.predict(data)[0]
-    presentase = max(model.predict_proba(data)[0])
-    
-    st.success(f"Hasil prediksi: **{prediksi}** dengan tingkat keyakinan **{presentase*100:.2f}%**")
+    data_baru = pd.DataFrame(
+        [[diameter, berat, tebal_kulit, kadar_gula, asal_daerah, musim_panen, warna]],
+        columns=["diameter", "berat", "tebal_kulit", "kadar_gula", "asal_daerah", "musim_panen", "warna"]
+    )
+    prediksi = model.predict(data_baru)[0]
+    presentase = max(model.predict_proba(data_baru)[0])
+    st.success(f"Model memprediksi {prediksi} dengan tingkat keyakinan {presentase*100:.2f}%")
     st.balloons()
+    st.snow()
 
 st.divider()
-st.caption("Dibuat dengan 🍋 oleh **Saffa Dhiya**")
+st.caption("Dibuat dengan :lemon: oleh DHHYAAAU")
